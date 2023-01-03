4ad1fae891d7d6a4caf394dc09b30c69dc3fa7f2
Michael Trank <mtrank@guident.co>
2023-01-03 17:36:28 -0500

    changed the typescript gmap api types package to be "google.maps"..... "googlemaps" is deprectated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import {} from 'googlemaps';
+import { } from 'google.maps';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,13 @@ import {} from 'googlemaps';
 
 export class AppComponent {
 
-  title = 'gmaps-with-ng';
+  title = 'Google Maps API in Angular Framework';
 
-  @ViewChild('nggooglemap') mapElement: any;
+  @ViewChild('nggooglemap') mapDivElement: any;
 
   googleMap: any;      // will be set to be a "google.maps.Map" object
   googleMapMarker: any;    // will be set to be a "google.maps.Marker" object
   googleMapMarkerInfoWindow: any;   // will be set to be a "google.maps.InfoWindow" object
-
 
 
   ngAfterViewInit() {
@@ -29,7 +28,7 @@ export class AppComponent {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    this.googleMap = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.googleMap = new google.maps.Map(this.mapDivElement.nativeElement, mapProperties);
     this.googleMapMarker = new google.maps.Marker({position: new google.maps.LatLng(26.2469577,-80.087821), map: this.googleMap});
     this.googleMapMarkerInfoWindow = new google.maps.InfoWindow({ content: "<br>Mike's House</br>" });
     this.googleMapMarkerInfoWindow.open({ anchor: this.googleMapMarker, map: this.googleMap, shouldFocus: false });
